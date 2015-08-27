45fe5ada94dd70b9c56dac7028ca6f521ac62c1c
Stephen Davies <stephen@umw.edu>
2015-08-27 00:34:14 -0400

    Collect tables; combine differently.

--- server.R
+++ server.R
@@ -5,13 +5,19 @@ library(xtable)
 
 shinyServer(function(input,output,session) {
 
+compute.level <- function(xp) {
+    if (xp < 5) { return("Suck") } else { return("nice") }
+}
+
     output$xpPlot <- renderTable({
 
         db.src <- src_mysql("stephen",user="stephen",password="davies4ever")
-        chars <- tbl(db.src, "chars")
-        xp <- tbl(db.src, "xp")
-        display <- left_join(chars,xp,by=c("charname"="username")) %>% 
-            group_by(username) %>% summarize(XP=sum(xp))
+        chars <- collect(tbl(db.src, "chars"))
+        xp <- collect(tbl(db.src, "xp"))
+        display <- inner_join(chars,xp,by=c("charname"="username")) %>% 
+            group_by(charname) %>% 
+            summarize(XP=sum(xp), compute.level(XP), latest=max(thetime)) %>%
+            arrange(desc(XP))
         xtable(as.data.frame(display))
     })
 
@@ -25,6 +31,10 @@ shinyServer(function(input,output,session) {
                         paste0("insert into chars values (",
                         "'",input$realname,"',",
                         "'",input$charname,"')")
+                    )
+                    dbGetQuery(get.connection(),
+                        paste0("insert into xp values (",
+                        "'",input$charname,"',1,'sign-up',now())")
                     )
                     "Account created!"
                 }, error=function(e) paste0("Could not create account! ",
