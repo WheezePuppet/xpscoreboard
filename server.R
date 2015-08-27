c4917275889cac34427750cae71568b0c6b0ed95
Stephen Davies <stephen@umw.edu>
2015-08-27 11:37:53 -0400

    Make left panel reactive. (& touchups.)

--- server.R
+++ server.R
@@ -17,20 +17,21 @@ compute.level <- function(xp) {
                 "Inferior Adventurer"=700,
                 "Deficient Adventurer"=600,
                 "Inadequate Adventurer"=500,
-                "Non-Adventurer"=0)
+                "n00b"=0)
                 
     names(levels[xp > levels][1])
 }
 
     output$xpPlot <- renderTable({
 
+        input$addchar
         db.src <- src_mysql("stephen",user="stephen",password="davies4ever")
         chars <- collect(tbl(db.src, "chars"))
         xp <- collect(tbl(db.src, "xp"))
         display <- inner_join(chars,xp,by=c("charname"="username")) %>% 
             group_by(charname) %>% 
-            summarize(XP=sum(xp), Level=compute.level(XP), 
-                "Most recent"=max(thetime)) %>%
+            summarize(Level=compute.level(sum(xp)), XP=sum(xp), 
+                "Most recent experience"=max(thetime)) %>%
             arrange(desc(XP))
         xtable(as.data.frame(display))
     })
