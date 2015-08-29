a40181ca81332f1ca22a5fc80bb69490878e4445
Stephen Davies <stephen@umw.edu>
2015-08-29 08:05:33 -0400

    Whoops, forgot to remove password.

--- mysql_config.R
+++ mysql_config.R
@@ -4,9 +4,5 @@ mysql.db.name <- "stephen"
 mysql.user <- "joeblow"
 mysql.password <- "zepassword"
 
-mysql.user <- "stephen"
-mysql.password <- "davies4ever"
-
-
 # git update-index --assume-unchanged mysql_config.R
 
