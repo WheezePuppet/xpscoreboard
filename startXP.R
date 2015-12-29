75fce4fff702d566a6b6abd833d7077783eec388
Stephen <stephen@umw.edu>
2015-12-28 23:11:26 -0500

    Add note to keep startXP.R non-updated in general.

--- startXP.R
+++ startXP.R
@@ -1,3 +1,6 @@
 
 require(shiny)
-runApp(appDir="/home/stephen/xpscoreboard",port=22228,launch.browser=FALSE,host="199.111.89.20")
+runApp(appDir="/home/stephen/420xpscoreboard",port=22227,launch.browser=FALSE,host="199.111.89.20")
+
+# git update-index --assume-unchanged startXP.R
+
